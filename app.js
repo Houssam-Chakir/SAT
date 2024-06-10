0e357a06f9a65435bcafc3d852a41e2e5ea4b9d4
houssam chakir <thisischchannel@gmail.com>
2024-06-10 11:11:48 +0100

    form logic progress

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 import Dropdown, { dropDownBtn, dropDownItems } from "./src/dropdown.js";
 import { fp } from "./src/datePicker.js";
 import caseSection from "./src/caseSection.js";
-import subAdvisors, { saDropDownBtn } from "./src/subAdvisors.js";
+import subAdvisors, { subAdvisorsSection } from "./src/subAdvisors.js";
 
 //Event Listeners
 dropDownBtn.addEventListener("click", () => Dropdown.showMenu());
 dropDownItems.forEach((item) => {
   item.addEventListener("click", () => {
-    caseSection.updateShift(item.id)
-    Dropdown.showMenu()
+    caseSection.updateShift(item.id);
+    Dropdown.showMenu();
   });
 });
-saDropDownBtn.addEventListener('click', )
+subAdvisorsSection.addEventListener("click", (e) => {
+
+  if (
+    e.target.id === "menu-button__SA" ||
+    e.target.parentElement.id === "menu-button__SA"
+  )
+    console.log('works');
+});
 
 // init flatPicker
 fp();
-subAdvisors.formGenerator()
+subAdvisors.formGenerator();
--- src/subAdvisors.js
+++ src/subAdvisors.js
@@ -1,5 +1,5 @@
-const subAdvisorsSection = document.querySelector('.Sub-advisors')
-export const saDropDownBtn = subAdvisorsSection.querySelector('.drop-down__btn')
+export const subAdvisorsSection = document.querySelector('.Sub-advisors')
+
 
 class SubAdvisors {
   subAdvisors = ['Brahim', 'Ahmed', 'Ayman', 'Halima', 'Benhida']
@@ -7,15 +7,15 @@ class SubAdvisors {
   formGenerator() {
     const markup = `
       <!-- SUB ADVISOR STAT -->
-      <div class="Sub-advisor flex gap-3">
-        <div>Sub-advisor:</div>
+      <div class="Sub-advisor flex gap-3 mx-3">
+        <div class="pt-1">Sub-advisor:</div>
         <div class="relative inline-block text-left">
           <!-- SUB ADVISORS DROPDOWN BUTTON -->
           <div class="drop-down__btn">
             <button type="button"
               class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              id="menu-button__SB" aria-expanded="true" aria-haspopup="true">
-              Shift
+              id="menu-button__SA" aria-expanded="true" aria-haspopup="true">
+              choose a name
               <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd"
                   d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
@@ -35,13 +35,20 @@ class SubAdvisors {
           </div>
 
         </div>
+        <!-- CASE COUNT -->
+        <div class="flex gap-3">
+          <div class="pt-1">Case count</div>
+          <input id="caseCount" type="number" class="text-blue-950 rounded-md px-2 w-[70px]" placeholder="">
+        </div>
         <!-- AHT -->
-        <div class="pt-1">AHT</div>
-        <input type="number" class="text-blue-950 rounded-md px-2" placeholder="in Minutes">
-
+        <div class="flex gap-3">
+          <div class="pt-1">AHT</div>
+          <input id="AHT" type="number" class="text-blue-950 rounded-md px-2 w-[120px]" placeholder="in Minutes">
+        </div>
       </div>
     `
     subAdvisorsSection.insertAdjacentHTML('beforeend', markup)
+    console.log('form added');
   }
 
   subAdvisorslistGenerator() {
