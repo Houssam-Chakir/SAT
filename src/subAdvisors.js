37fb94bf558d64fdac6202f6de15a2a68d206962
houssam chakir <thisischchannel@gmail.com>
2024-07-16 13:06:37 +0100

    fixed delete button removed after choosing a name

--- src/subAdvisors.js
+++ src/subAdvisors.js
@@ -79,8 +79,12 @@ class SubAdvisors {
     //drop down items on click of each
     dropDown.addEventListener("click", (e) => {
       btn.innerHTML = `
+      <!-- Delete BUTTON -->
+      <button class='Sub-advisor__delete-btn bg-red-800 hover:bg-red-500 w-10 h-10 p-2 rounded-s-md ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-red-200'>
+        <svg class='rotate-45' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="#d4d4d4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      </button>
       <button data-name='${e.target.id}' type="button"
-      class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-gray-950 px-3 py-2  text-sm font-medium text-white shadow-sm ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-slate-200"
+      class="inline-flex w-full justify-center gap-x-1.5 rounded-e-md bg-gray-950 px-3 py-2  text-sm font-medium text-white shadow-sm ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-slate-200"
       id="menu-button__SA" aria-expanded="true" aria-haspopup="true">
       ${e.target.id}
       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
