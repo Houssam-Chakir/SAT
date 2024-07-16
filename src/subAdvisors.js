884d816eea19cec0472da9fe6ec1fc119e6afa61
houssam chakir <thisischchannel@gmail.com>
2024-07-16 12:49:18 +0100

    delete button moved to be attached to name button and some padding changes

--- src/subAdvisors.js
+++ src/subAdvisors.js
@@ -12,10 +12,14 @@ class SubAdvisors {
       <!-- SUB ADVISOR STAT -->
       <div class="Sub-advisor flex gap-3">
         <div class="relative inline-block text-left">
-          <!-- SUB ADVISORS DROPDOWN BUTTON -->
-          <div class="drop-down__btn">
+          <div class="drop-down__btn flex">
+            <!-- Delete BUTTON -->
+            <button class='Sub-advisor__delete-btn bg-red-800 hover:bg-red-500 w-10 h-10 p-2 rounded-s-md ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-red-200'>
+              <svg class='rotate-45' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="#d4d4d4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            </button>
+            <!-- SUB ADVISORS DROPDOWN BUTTON -->
             <button  data-name='${sa ? sa : ''}' type="button"
-              style="z-index: ${this.zIndex};" class=" inline-flex w-full justify-center gap-x-1.5 rounded-md bg-gray-950 px-3 py-2  text-sm font-medium ${sa ? 'text-white' : 'text-gray-400'} shadow-sm ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-slate-200"
+              style="z-index: ${this.zIndex};" class=" inline-flex w-full justify-center gap-x-1.5 rounded-e-md bg-gray-950 px-3 py-2 h-10  text-sm font-medium ${sa ? 'text-white' : 'text-gray-400'} shadow-sm ring-1 ring-inset ring-gray-700 hover:ring-2 hover:ring-slate-200"
               id="menu-button__SA" aria-expanded="true" aria-haspopup="true">
               ${sa ? sa : 'Select a name'}
               <svg class="mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -41,7 +45,7 @@ class SubAdvisors {
         </div>
         <!-- HT -->
         <div class="flex">
-          <label class="pt-1 pe-2">Handle Time</label>
+          <label class="pt-1 pe-2 mt-1 ms-1">Handle Time</label>
           <button type="button" onclick="this.parentNode.querySelector('[type=number]').stepDown();" class='bg-slate-800 hover:bg-slate-500 w-8 p-2 rounded-s-md ring-1 ring-inset ring-gray-700'>
             -
           </button>
@@ -51,10 +55,7 @@ class SubAdvisors {
 
           </button>
         </div>
-        <!-- Delete BUTTON -->
-        <button class='Sub-advisor__delete-btn bg-red-800 hover:bg-red-500 w-8 h-8 p-2 rounded-xl mt-[4px] ms-4'>
-          <svg class='rotate-45' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="#d4d4d4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-        </button>
+
       </div>
     `;
     subAdvisorsSection.firstElementChild.insertAdjacentHTML("beforeend", markup);
